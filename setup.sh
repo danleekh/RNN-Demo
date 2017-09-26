f89fb53c51e8e79c713700a2b947efb2edcecb99
davidtan <davidtan@thoughtworks.com>
2017-09-26 15:30:17 +0800

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# set -e
-
-which python3
-if [ $? -ne 0 ]; then
-  echo "INFO: Installing python 3"
-  brew install python3
-fi
-
-if [ ! -d .venv ]; then
-  echo "INFO: Creating virtual env folder in current directory"
-  python3 -m venv .venv
-fi
-
-echo "Activating virtual environment"
+python3 -m venv .venv
 source .venv/bin/activate
-
-pip3 install -r requirements.txt
-
+pip install -r requirements.txt
